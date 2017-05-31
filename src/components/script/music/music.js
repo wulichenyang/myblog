37f4624c08c4001baf7ac91a909ca27c6536d13b
wulichenyang <943934049@qq.com>
2017-05-31 00:20:17 +0000

    finish music component

--- src/components/script/music/music.js
+++ src/components/script/music/music.js
@@ -1,4 +1,79 @@
 angular.module('music', [])
   .controller('musicCtrl', ['$scope', ($scope) => {
     
+    $scope.musicList = [
+      [
+        {
+          songName: '枫',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '一路向北',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '最长的电影',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '发如雪',
+          singerName: '李晨阳'
+        },
+      ],
+      [
+        {
+          songName: '枫',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '一路向北',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '最长的电影',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '发如雪',
+          singerName: '李晨阳'
+        },
+      ],
+      [
+        {
+          songName: '枫',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '一路向北',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '最长的电影',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '发如雪',
+          singerName: '李晨阳'
+        },
+      ],
+      [
+        {
+          songName: '枫',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '一路向北',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '最长的电影',
+          singerName: '李晨阳'
+        },
+        {
+          songName: '发如雪',
+          singerName: '李晨阳'
+        },
+      ],
+    ]
+
   }])
--- src/components/sharedPlugins/ui.js
+++ src/components/sharedPlugins/ui.js
@@ -35,3 +35,66 @@ angular.module('sharedPlugins.ui', [])
     `,
   }
 }])
+
+.directive('songListItem', [() => {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      data: '=',
+			index: '=',
+    },
+    template: `
+			<li class="song-item">
+				<span class="song-number">{{index}}</span>
+				<a ng-click="" class="song-info">
+					<h4>{{data.songName}}</h4>
+					<h5>{{data.singerName}}</h5>
+				</a>
+			</li>
+    `,
+  }
+}])
+
+.directive('musicListItem', [() => {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      data: '=',
+			index: '=',
+    },
+    template: `
+			<li class="music-list-item col-sm-6 col-md-3">
+				<div class="music-content">
+					<div class="music-item-bg" ng-class="bgPosition(index)"></div>
+					<i class="music-item-mask"></i>
+					<i class="music-play-icon"></i>
+					<i class="music-line"></i>
+					<div class="music-item-header">
+						<a href="">
+							<h4>巅峰榜</h4>
+							<h3>流行指数</h3>
+						</a>
+					</div>
+					<ul class="song-list">
+
+						<song-list-item ng-repeat="data in data track by $index" data="data" index="$index + 1"></song-list-item>
+						
+					</ul>
+				</div>
+			</li>
+    `,
+		link: (scope, elem, attr) => {
+			scope.bgPosition = index => {
+				if(2 === index) {
+					return 'bg-position2'
+				} else if (3 === index) {
+					return 'bg-position3'
+				} else if (4 === index){
+					return 'bg-position4'
+				} 
+			}
+		}
+  }
+}])
